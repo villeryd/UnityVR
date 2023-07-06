e27e3b64f72222a83c7d68e6c7a7bd0417e409cd
Daniel Villery <danielvillery@gmail.com>
2023-07-06 19:45:17 -0400

    pistol created

---
+++ Assets/DetectVR.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.XR.Management
+
+public class DetectVR : MonoBehaviour
+{
+   public GameObject xrOrigin;
+   public GameObject desktopCharacter;
+
+    void Start()
+    {
+        var xrSettings = XRGeneralSettings
+    }
+
+   
+}
