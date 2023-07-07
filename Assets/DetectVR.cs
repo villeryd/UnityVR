92209abc2aca2aa0505f488697dda8b6ab3fa462
Daniel Villery <danielvillery@gmail.com>
2023-07-06 21:04:28 -0400

    gun is grabable

--- Assets/DetectVR.cs
+++ Assets/DetectVR.cs
@@ -1,15 +1,13 @@
 using UnityEngine;
-using UnityEngine.XR.Management
+using UnityEngine.XR.Management;
 
 public class DetectVR : MonoBehaviour
 {
-   public GameObject xrOrigin;
-   public GameObject desktopCharacter;
+    public GameObject xrOrigin;
+    public GameObject desktopCharacter;
 
     void Start()
     {
-        var xrSettings = XRGeneralSettings
+        //var xrSettings = XRGeneralSettings;
     }
-
-   
 }
