98d3d8e9833d5cac249c21110b424d43bc522b1c
Daniel Villery <danielvillery@gmail.com>
2023-07-07 00:37:07 -0400

    first commit

--- Assets/FireBulletOnActivate.cs
+++ Assets/FireBulletOnActivate.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
+
 public class FireBulletOnActivate : MonoBehaviour
 {
     public GameObject bullet;
